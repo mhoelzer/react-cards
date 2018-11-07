a0e8881aa9cbbec01fa607d265412715aedc2277
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-07 15:33:15 -0500

    some cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 
-/*
- * CardGroup
- * This component takes Card components as children and renders them.
- * Example usage:
- *
- *      <CardGroup>
- *          <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
- *      </CardGroup>
- *
- * The above would render a card group with a single child in it.
- *
- */
-// ASK WHY the props here are in (), but in the readme, they dont have them
+// CardGroup
+// This component takes Card components as children and renders them.
+// Example usage:
+//      <CardGroup>
+//          <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
+//      </CardGroup>
+// The above would render a card group with a single child in it.
+
 const CardGroup = (props) => (
     <div className="cardGroup">
         {props.children}
     </div>
 );
 
+// subcat of cardgroup
 const Card = (props) => (
     <div className="card cardGroup__card">
         <div className="card__description cardGroup__cardDescription">
             <div className={`icon fa ${props.icon} card__descriptionIcon`}></div>
             <div className="card__descriptionText">
                 {props.description}
-                <br/>
+                {/* null is absense of obj, and react likes null if not there */}
+                {props.hint && <br/>}
                 {props.hint && <span>{props.hint}</span>}
             </div>
         </div>
@@ -38,37 +35,6 @@ const App = () => (
         <Card icon="fa-thumbs-o-up" description="Trial" price="Free!"/>
         <Card icon="fa-trophy" description="Basic tier" hint="(most popular)" price="$10.00"/>
         <Card icon="fa-bolt" description="Advanced tier" hint="(only for enterprise-level professionals)" price="$6,000.00"/>
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Trial
-                </div>
-            </div>
-            <div className="card__price">Free!</div>
-        </div> */}
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-trophy card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Basic tier
-                    <br/>
-                    (most popular)
-                </div>
-            </div>
-            <div className="card__price">$10.00</div>
-        </div> */}
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-bolt card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Advanced tier
-                    <br/>
-                    (only for enterprise-level professionals)
-                </div>
-            </div>
-            <div className="card__price">$6,000.00</div>
-        </div> */}
     </CardGroup>
 );
 
